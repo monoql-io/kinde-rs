905d2db79f09d8fd841f448e22ad38ed007a83bf
ielm <ivan.leon@guidanceanalytics.com>
2023-11-24 13:27:24 -0800

    use main branch

--- git_push.sh
+++ git_push.sh
@@ -50,7 +50,7 @@ if [ "$git_remote" = "" ]; then # git remote not defined
 
 fi
 
-git pull origin master
+git pull origin main
 
 # Pushes (Forces) the changes in the local repository up to the remote repository
 echo "Git pushing to https://${git_host}/${git_user_id}/${git_repo_id}.git"
